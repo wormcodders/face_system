1fed06dc21f2f3de525440ef2e5b5e76c9c1932b
wormcodders <booksellers166@gmail.com>
2025-08-29 00:14:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import yaml
 from gui import launch_app
 
+# Load config
+with open("config.yaml", "r") as f:
+    CONFIG = yaml.safe_load(f)
+
 if __name__ == "__main__":
-    launch_app()
+    launch_app(CONFIG)
